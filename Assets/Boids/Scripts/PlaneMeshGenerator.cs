19d2d15e1d34934a2d2d97acfe1115de77f019c8
Pixum <pixum2001@gmail.com>
2022-04-22 14:53:42 +0200

    Added Grass Shader

---
+++ Assets/Boids/Scripts/PlaneMeshGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMeshGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Resolution;
+    [SerializeField]
+    private Vector2 m_Dimensions;
+
+
+}
